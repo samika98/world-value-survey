7f3e86726083fa2bba42b6d4595539c67f78ad56
Samika Kashyap <samikakashyap@Samikas-MacBook-Air.local>
2022-04-23 22:46:46 -0400

    initial commit

---
+++ html/data/population.js
@@ -0,0 +1,708 @@
+var svgMapDataPopulation = { 
+  data: {
+    DoNotKnow: {
+      name: 'DoNotKnow',
+      format: '{0} km<sup>2</sup>',
+      thousandSeparator: ','
+    },
+    VeryImportant: {
+      name: 'VeryImportant',
+      thousandSeparator: ','
+    },
+    NotVeryImportant: {
+      name: 'NotVeryImportant',
+      thousandSeparator: ',',
+  
+    },
+    NoAnswer: {
+      name: 'NoAnswer',
+      thousandSeparator: ','
+    },
+    NotAtAllImportant: {
+      name: 'NotAtAllImportant',
+      thousandSeparator: ','
+    },
+    RatherImportant: {
+      name: 'RatherImportant',
+      RatherImportant: ','
+    }
+  },
+  applyData: 'VeryImportant',
+  values: {
+    
+      DE: {
+        DoNotKnow : 0,
+        VeryImportant : 88,
+        NotVeryImportant :  1.4,
+        NoAnswer : 0.5,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 10
+      },
+      PR: {
+        DoNotKnow : 0,
+        VeryImportant : 93.4,
+        NotVeryImportant :  0.8,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.4,
+        RatherImportant : 5.2
+      },
+      TW: {
+        DoNotKnow : 0,
+        VeryImportant : 90.8,
+        NotVeryImportant :  0.6,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 8.6
+      },
+      HK: {
+        DoNotKnow : 0,
+        VeryImportant : 78.7,
+        NotVeryImportant :  1.6,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 19.5
+      },
+      PT: {
+        DoNotKnow : 0,
+        VeryImportant : 87.8,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 11.5
+      },
+      DK: {
+        DoNotKnow : 0.1,
+        VeryImportant : 87.6,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 11.2
+      },
+      LT: {
+        DoNotKnow : 0.5,
+        VeryImportant : 66.7,
+        NotVeryImportant :  3.6,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 28.7
+      },
+      HR: {
+        DoNotKnow : 0.1,
+        VeryImportant : 81.8,
+        NotVeryImportant :  1.2,
+        NoAnswer : 0.2,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 16.6
+      },
+      UA: {
+        DoNotKnow : 0.3,
+        VeryImportant : 85,
+        NotVeryImportant :  1.3,
+        NoAnswer : 0.2,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 13
+      },
+      HU: {
+        DoNotKnow : 0,
+        VeryImportant : 90,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 9
+      },
+      LY: {
+        DoNotKnow : 0.1,
+        VeryImportant : 98.1,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 1.6
+      },
+      MA: {
+        DoNotKnow : 0,
+        VeryImportant : 89.1,
+        NotVeryImportant :  1.1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 9.7
+      },
+      ME: {
+        DoNotKnow : 0,
+        VeryImportant : 88.8,
+        NotVeryImportant :  1.9,
+        NoAnswer : 0.2,
+        NotAtAllImportant : 0.5,
+        RatherImportant : 8.7
+      },
+      ID: {
+        DoNotKnow : 0,
+        VeryImportant : 98,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 1.5
+      },
+      EC: {
+        DoNotKnow : 0,
+        VeryImportant : 87.4,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0,
+        RatherImportant : 12.2
+      },
+      US: {
+        DoNotKnow : 0,
+        VeryImportant : 91,
+        NotVeryImportant :  1.6,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 7.1
+      },
+      MM: {
+        DoNotKnow : 0,
+        VeryImportant : 86.6,
+        NotVeryImportant :  0.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 12.8
+      },
+      EE: {
+        DoNotKnow : 0.3,
+        VeryImportant : 84.8,
+        NotVeryImportant :  1.4,
+        NoAnswer : 0.5,
+        NotAtAllImportant : 0.7,
+        RatherImportant : 12.2
+      },
+      MN: {
+        DoNotKnow : 0,
+        VeryImportant : 81.4,
+        NotVeryImportant :  3.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.6,
+        RatherImportant : 14.7
+      },
+      EG: {
+        DoNotKnow : 0,
+        VeryImportant : 99.7,
+        NotVeryImportant :  0,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 0.3
+      },
+      AD: {
+        DoNotKnow : 0,
+        VeryImportant : 89.5,
+        NotVeryImportant :  0.5,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 9.7
+      },
+      IQ: {
+        DoNotKnow : 0,
+        VeryImportant : 97.2,
+        NotVeryImportant :  0.5,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 2.2
+      },
+      IR: {
+        DoNotKnow : 0,
+        VeryImportant : 93.9,
+        NotVeryImportant :  0.8,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 5.2
+      },
+      IS: {
+        DoNotKnow : 0,
+        VeryImportant : 95.3,
+        NotVeryImportant :  0.6,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 4
+      },
+      AL: {
+        DoNotKnow : 0.1,
+        VeryImportant : 97.4,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0.2,
+        NotAtAllImportant : 0,
+        RatherImportant : 1.9
+      },
+      IT: {
+        DoNotKnow : 0,
+        VeryImportant : 90.1,
+        NotVeryImportant :  0.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 9.5
+      },
+      MX: {
+        DoNotKnow : 0,
+        VeryImportant : 97.1,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 2.5
+      },
+      AM: {
+        DoNotKnow : 0,
+        VeryImportant : 91.7,
+        NotVeryImportant :  0.5,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 7.5
+      },
+      MY: {
+        DoNotKnow : 0,
+        VeryImportant : 96.3,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 3.4
+      },
+      ES: {
+        DoNotKnow : 0,
+        VeryImportant : 87.3,
+        NotVeryImportant :  0.9,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 11.7
+      },
+      ET: {
+        DoNotKnow : 0,
+        VeryImportant : 94.9,
+        NotVeryImportant :  0.1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 5
+      },
+      VE: {
+        DoNotKnow : 0,
+        VeryImportant : 83.4,
+        NotVeryImportant :  0.8,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 15.7
+      },
+      AR: {
+        DoNotKnow : 0,
+        VeryImportant : 91.7,
+        NotVeryImportant :  0.1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 8.2
+      },
+      AT: {
+        DoNotKnow : 0,
+        VeryImportant : 82.3,
+        NotVeryImportant :  3.4,
+        NoAnswer : 0.2,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 13.8
+      },
+      AU: {
+        DoNotKnow : 0,
+        VeryImportant : 90.2,
+        NotVeryImportant :  1.5,
+        NoAnswer : 0.3,
+        NotAtAllImportant : 0.6,
+        RatherImportant : 7.4
+      },
+      VN: {
+        DoNotKnow : 0,
+        VeryImportant : 96.8,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 2.6
+      },
+      AZ: {
+        DoNotKnow : 0.1,
+        VeryImportant : 90.7,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0,
+        RatherImportant : 8.4
+      },
+      NG: {
+        DoNotKnow : 0,
+        VeryImportant : 98.4,
+        NotVeryImportant :  0.1,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0,
+        RatherImportant : 1.5
+      },
+      NI: {
+        DoNotKnow : 0,
+        VeryImportant : 77.8,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 21.8
+      },
+      RO: {
+        DoNotKnow : 0.1,
+        VeryImportant : 90.7,
+        NotVeryImportant :  1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 8
+      },
+      ZW: {
+        DoNotKnow : 0,
+        VeryImportant : 96.1,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 3.5
+      },
+      NL: {
+        DoNotKnow : 0.8,
+        VeryImportant : 79.3,
+        NotVeryImportant :  4.4,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 1.5,
+        RatherImportant : 13.9
+      },
+      BA: {
+        DoNotKnow : 0.1,
+        VeryImportant : 92.4,
+        NotVeryImportant :  0.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 7.1
+      },
+      NO: {
+        DoNotKnow : 0,
+        VeryImportant : 92.2,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 7.3
+      },
+      RS: {
+        DoNotKnow : 0.1,
+        VeryImportant : 91,
+        NotVeryImportant :  0.5,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 7.9
+      },
+      BD: {
+        DoNotKnow : 0,
+        VeryImportant : 98.6,
+        NotVeryImportant :  0.1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 1.3
+      },
+      FI: {
+        DoNotKnow : 0.4,
+        VeryImportant : 83.4,
+        NotVeryImportant :  1.5,
+        NoAnswer : 0.4,
+        NotAtAllImportant : 0.7,
+        RatherImportant : 13.6
+      },
+      RU: {
+        DoNotKnow : 0.3,
+        VeryImportant : 80.5,
+        NotVeryImportant :  2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.5,
+        RatherImportant : 16.6
+      },
+      BG: {
+        DoNotKnow : 0.3,
+        VeryImportant : 87.1,
+        NotVeryImportant :  1.2,
+        NoAnswer : 0.5,
+        NotAtAllImportant : 0,
+        RatherImportant : 10.9
+      },
+      JO: {
+        DoNotKnow : 0,
+        VeryImportant : 97.9,
+        NotVeryImportant :  0,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 1.9
+      },
+      JP: {
+        DoNotKnow : 0.1,
+        VeryImportant : 92,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0.7,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 6.2
+      },
+      FR: {
+        DoNotKnow : 0,
+        VeryImportant : 85.4,
+        NotVeryImportant :  2.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.6,
+        RatherImportant : 11.6
+      },
+      NZ: {
+        DoNotKnow : 0,
+        VeryImportant : 87.2,
+        NotVeryImportant :  1.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.5,
+        RatherImportant : 7
+      },
+      BO: {
+        DoNotKnow : 0,
+        VeryImportant : 80.9,
+        NotVeryImportant :  1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 17.9
+      },
+      BR: {
+        DoNotKnow : 0,
+        VeryImportant : 85.3,
+        NotVeryImportant :  1.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.4,
+        RatherImportant : 13
+      },
+      SE: {
+        DoNotKnow : 0.3,
+        VeryImportant : 90.7,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0.5,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 7.6
+      },
+      SG: {
+        DoNotKnow : 0,
+        VeryImportant : 92,
+        NotVeryImportant :  0.9,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 7
+      },
+      SI: {
+        DoNotKnow : 0.3,
+        VeryImportant : 84.8,
+        NotVeryImportant :  1.3,
+        NoAnswer : 0.4,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 13
+      },
+      BY: {
+        DoNotKnow : 0.3,
+        VeryImportant : 88.7,
+        NotVeryImportant :  0.8,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.4,
+        RatherImportant : 9.7
+      },
+      SK: {
+        DoNotKnow : 0,
+        VeryImportant : 90.7,
+        NotVeryImportant :  0.6,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 8.7
+      },
+      KE: {
+        DoNotKnow : 0,
+        VeryImportant : 97.1,
+        NotVeryImportant :  0.8,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 2.1
+      },
+      GB: {
+        DoNotKnow : 0,
+        VeryImportant : 90.7,
+        NotVeryImportant :  1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 7.9
+      },
+      KG: {
+        DoNotKnow : 0,
+        VeryImportant : 92.4,
+        NotVeryImportant :  0.1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 7.5
+      },
+      CA: {
+        DoNotKnow : 0,
+        VeryImportant : 79.3,
+        NotVeryImportant :  3.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.7,
+        RatherImportant : 16.8
+      },
+      GE: {
+        DoNotKnow : 0.1,
+        VeryImportant : 90.6,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 8.9
+      },
+      CH: {
+        DoNotKnow : 0,
+        VeryImportant : 85.7,
+        NotVeryImportant :  2.1,
+        NoAnswer : 0.9,
+        NotAtAllImportant : 0.4,
+        RatherImportant : 10.9
+      },
+      KR: {
+        DoNotKnow : 0,
+        VeryImportant : 88.9,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 10.8
+      },
+      CL: {
+        DoNotKnow : 0,
+        VeryImportant : 91.4,
+        NotVeryImportant :  1.5,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 7.1
+      },
+      CN: {
+        DoNotKnow : 0,
+        VeryImportant : 86.2,
+        NotVeryImportant :  0.6,
+        NoAnswer : 0.3,
+        NotAtAllImportant : 0,
+        RatherImportant : 13
+      },
+      GR: {
+        DoNotKnow : 0,
+        VeryImportant : 94.1,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.4,
+        RatherImportant : 5.3
+      },
+      CO: {
+        DoNotKnow : 0,
+        VeryImportant : 80.7,
+        NotVeryImportant :  0.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.4,
+        RatherImportant : 18.6
+      },
+      GT: {
+        DoNotKnow : 0,
+        VeryImportant : 87.3,
+        NotVeryImportant :  0.6,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 12
+      },
+      KZ: {
+        DoNotKnow : 0,
+        VeryImportant : 93.5,
+        NotVeryImportant :  0.5,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 6
+      },
+      TH: {
+        DoNotKnow : 0.1,
+        VeryImportant : 90.5,
+        NotVeryImportant :  0.9,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 1.3,
+        RatherImportant : 7
+      },
+      PE: {
+        DoNotKnow : 0,
+        VeryImportant : 87.8,
+        NotVeryImportant :  0.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 11.8
+      },
+      CY: {
+        DoNotKnow : 0,
+        VeryImportant : 94.2,
+        NotVeryImportant :  0.7,
+        NoAnswer : 0.2,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 4.9
+      },
+      LB: {
+        DoNotKnow : 0,
+        VeryImportant : 96.8,
+        NotVeryImportant :  0.1,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 3.1
+      },
+      TJ: {
+        DoNotKnow : 0,
+        VeryImportant : 95.9,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 3.9
+      },
+      CZ: {
+        DoNotKnow : 0.1,
+        VeryImportant : 86,
+        NotVeryImportant :  1.9,
+        NoAnswer : 0.3,
+        NotAtAllImportant : 0.5,
+        RatherImportant : 11.3
+      },
+      PH: {
+        DoNotKnow : 0,
+        VeryImportant : 98.2,
+        NotVeryImportant :  0.2,
+        NoAnswer : 0,
+        NotAtAllImportant : 0,
+        RatherImportant : 1.6
+      },
+      TN: {
+        DoNotKnow : 0,
+        VeryImportant : 97.4,
+        NotVeryImportant :  0.4,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.1,
+        RatherImportant : 2.1
+      },
+      PK: {
+        DoNotKnow : 0.1,
+        VeryImportant : 92.6,
+        NotVeryImportant :  0.9,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.5,
+        RatherImportant : 6
+      },
+      PL: {
+        DoNotKnow : 0.3,
+        VeryImportant : 93.3,
+        NotVeryImportant :  0.3,
+        NoAnswer : 0.1,
+        NotAtAllImportant : 0.3,
+        RatherImportant : 5.7
+      },
+      TR: {
+        DoNotKnow : 0,
+        VeryImportant : 91.8,
+        NotVeryImportant :  0.6,
+        NoAnswer : 0,
+        NotAtAllImportant : 0.2,
+        RatherImportant : 7.4
+      }
+    }
+  
+    
+};
